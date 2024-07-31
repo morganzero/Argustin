3b0856715f9bb76da2e5d4930023f635af5e9e9a
Morgan Zero <57449785+morganzero@users.noreply.github.com>
2024-07-31 12:14:31 +0200

    Update index.js

--- frontend/public/src/index.js
+++ frontend/public/src/index.js
@@ -1,1 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css'; // You can create an index.css for global styles
+import App from './App';
 
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
